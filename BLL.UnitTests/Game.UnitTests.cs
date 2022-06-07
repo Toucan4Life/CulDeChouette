d37a08a62d3088f7e2c504d4d7b94fbd222c403e
Toucan4Life <tdp94@hotmail.com>
2022-06-07 16:10:39 +0200

    First Rule implemented : Chouette

--- BLL.UnitTests/Game.UnitTests.cs
+++ BLL.UnitTests/Game.UnitTests.cs
@@ -1,16 +1,127 @@
 using System;
-using BLL.UnitTests;
+using System.Collections.Generic;
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace TestProject1
+namespace BLL.UnitTests
 {
     [TestClass]
     public class UnitTest1
     {
         [TestMethod]
-        public void Game_Should_Have_At_Least_2_Players()
+        public void Game_Should_Throw_Exception_When_Less_Than_2_Players()
         {
             Assert.ThrowsException<ArgumentException>(()=> new Game(1));
+        }
+
+        [TestMethod]
+        public void Game_Should_Throw_Exception_When_More_Than_6_Players()
+        {
+            Assert.ThrowsException<ArgumentException>(() => new Game(7));
+        }
+
+        [TestMethod]
+        public void New_Game_Should_Have_The_Specified_Players_Numbers()
+        {
+            var game = new Game(2);
+            Assert.AreEqual(2, game.Players.Count);
+        }
+
+        [TestMethod]
+        public void Players_Should_Number_Automatically_Attribued()
+        {
+            var game = new Game(2);
+            Assert.IsTrue(game.Players.Exists(t => t.Number == 1));
+            Assert.IsTrue(game.Players.Exists(t => t.Number == 2));
+        }
+
+        [TestMethod]
+        public void Players_Should_Start_With_Zero_Points()
+        {
+            var game = new Game(2);
+            Assert.AreEqual(0, game.Players.Single(t => t.Number == 1).Points);
+            Assert.AreEqual(0, game.Players.Single(t => t.Number == 2).Points);
+        }
+
+
+        [TestMethod]
+        public void Game_End_When_1_Player_Reach_343_Points()
+        {
+            var game = new Game(2);
+            game.Players.Single(t => t.Number == 1).Points = 343;
+
+            Assert.IsTrue(game.IsWon);
+        }
+
+        [TestMethod]
+        public void Game_End_When_1_Player_Is_Over_343_Points()
+        {
+            var game = new Game(2);
+            game.Players.Single(t => t.Number == 1).Points = 344;
+
+            Assert.IsTrue(game.IsWon);
+        }
+
+        [TestMethod]
+        public void Game_Should_Start_With_The_First_Player()
+        {
+            var game = new Game(2);
+
+            Assert.AreEqual(1, game.CurrentTurnPlayer.Number);
+        }
+
+        [TestMethod]
+        public void Second_Player_Should_Play_After_First_Player()
+        {
+            var game = new Game(2);
+            game.NextTurn();
+            Assert.AreEqual(2, game.CurrentTurnPlayer.Number);
+        }
+
+        [TestMethod]
+        public void First_Player_Should_Play_After_Last_Player()
+        {
+            var game = new Game(3);
+            game.NextTurn();
+            game.NextTurn();
+            game.NextTurn();
+            Assert.AreEqual(1, game.CurrentTurnPlayer.Number);
+        }
+
+        [TestMethod]
+        public void Game_Should_Start_At_Round_Zero()
+        {
+            var game = new Game(3);
+
+            Assert.AreEqual(1, game.CurrentRound);
+        }
+
+        [TestMethod]
+        public void After_All_Players_Turn_Round_Is_Increased()
+        {
+            var game = new Game(3);
+            game.NextTurn();
+            game.NextTurn();
+            game.NextTurn();
+            Assert.AreEqual(2, game.CurrentRound);
+        }
+
+        [TestMethod]
+        public void A_Chouette_Should_Earn_The_Players_Points()
+        {
+            var game = new Game(3);
+            game.NextTurn();
+            game.CalculateScore(new List<int>{1,2,2});
+            Assert.AreEqual(4, game.CurrentTurnPlayer.Points);
+        }
+
+        [TestMethod]
+        public void A_Chouette_Should_Earn_The_Players_Points_And_Add_Them_To_Current_Points()
+        {
+            var game = new Game(3);
+            game.CurrentTurnPlayer.Points = 3;
+            game.CalculateScore(new List<int> { 1, 2, 2 });
+            Assert.AreEqual(7, game.CurrentTurnPlayer.Points);
         }
     }
 }
--- BLL/Game.cs
+++ BLL/Game.cs
@@ -1,12 +1,64 @@
-namespace BLL.UnitTests;
+namespace BLL;
 
 public class Game
 {
     public Game(int playersNumber)
     {
-        if (playersNumber < 2)
+        if (playersNumber is < 2 or > 6)
         {
             throw new ArgumentException($"Players count should be between 2 and 6. Actual player count: {playersNumber}");
         }
+
+        Players = Enumerable.Range(1, playersNumber).Select(i => new Player(i)).ToList();
+        CurrentTurnPlayer = Players.First();
+        CurrentRound = 1;
     }
+
+    public List<Player> Players { get; set; }
+    public bool IsWon => Players.Any(p => p.Points >= 343);
+    public Player CurrentTurnPlayer { get; set; }
+    public int CurrentRound { get; set; }
+
+    public void NextTurn()
+    {
+        if (Players.IndexOf(CurrentTurnPlayer) == Players.Count - 1)
+        {
+            NextRound();
+        }
+        else
+        {
+            CurrentTurnPlayer = Players.ElementAt(Players.IndexOf(CurrentTurnPlayer) + 1);
+        }
+    }
+
+    private void NextRound()
+    {
+        CurrentTurnPlayer = Players.First();
+        CurrentRound++;
+    }
+
+    public void CalculateScore(IEnumerable<int> dices)
+    {
+        if (dices.Count() != 3)
+        {
+            throw new ArgumentException($"There should only be 3 dices. Actual dice count : {dices}");
+        }
+
+        //Chouette
+        if (dices.GroupBy(t=>t).Count(t => t.Count()==2) !=0)
+        {
+            var chouette = dices.GroupBy(t => t).First(t => t.Count() == 2).Key;
+            CurrentTurnPlayer.Points += chouette * chouette;
+        }
+    }
+}
+
+public class Player
+{
+    public Player(int number)
+    {
+        Number = number;
+    }
+    public int Number { get; set; }
+    public int Points { get; set; }
 }
